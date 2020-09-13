ccdff9b17a6551324bb90bff7820b8364699e2df
perpointt <58557830+perpointt@users.noreply.github.com>
2020-09-13 22:16:25 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const nuts = new Nuts({
+        container: ".nuts",
+        color: "#000",
+        hover: "#ffad00",
+        transition: 1,
+        lettersAnimation: true,
+    })
+})
